d1bbc1f4f6329217070ac32fed84dbdb40b8e0d9
Steffen Deusch <steffen@deusch.me>
2025-02-05 11:37:08 +0100

    allow to configure which languages to register for

--- lib/makeup_syntect/application.ex
+++ lib/makeup_syntect/application.ex
@@ -7,10 +7,18 @@ defmodule MakeupSyntect.Application do
 
   alias Makeup.Registry
 
+  defp register_for_languages do
+    Application.get_env(:makeup_syntect, :register_for_languages, :all)
+  end
+
+  defp excluded_languages do
+    Application.get_env(:makeup_syntect, :excluded_languages, ["elixir", "erlang", "eex", "heex"])
+  end
+
   @impl true
   def start(_type, _args) do
     for %{name: name, codeblock_name: sanitized_name, extensions: extensions} <-
-          MakeupSyntect.Syntaxes.all() do
+          languages_to_register_for() do
       Registry.register_lexer(MakeupSyntect.Lexer,
         options: [language: name],
         names: [name, sanitized_name],
@@ -24,5 +32,19 @@ defmodule MakeupSyntect.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MakeupSyntect.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp languages_to_register_for do
+    all_languages = MakeupSyntect.Syntaxes.all()
+
+    case register_for_languages() do
+      :all ->
+        excluded_languages = excluded_languages()
+
+        Enum.reject(all_languages, fn %{codeblock_name: name} -> name in excluded_languages end)
+
+      languages ->
+        Enum.filter(all_languages, fn %{codeblock_name: name} -> name in languages end)
+    end
   end
 end
