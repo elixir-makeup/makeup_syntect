a44a85273ed37c9324ab275199a4a25ca2d696d9
Steffen Deusch <steffen@deusch.me>
2025-02-05 16:14:32 +0100

    relax Elixir requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MakeupSyntect.MixProject do
     [
       app: :makeup_syntect,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
